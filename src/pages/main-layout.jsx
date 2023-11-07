94c3d4e30c34b519700adee4b4a0d0ecac3df4da
Guy <guyelizarov@gmail.com>
2023-11-07 20:25:19 +0200

    adding dark mode button

--- src/pages/main-layout.jsx
+++ src/pages/main-layout.jsx
@@ -1,17 +1,33 @@
-import { Button, Center, Flex, Heading } from "@chakra-ui/react"
+import { Button, ButtonGroup, Center, Flex, Heading, IconButton, useColorMode } from "@chakra-ui/react"
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 import { useNavigate } from "react-router-dom"
 import { Outlet } from "react-router-dom"
 
 export const MainLayout = () => {
     const navigate = useNavigate()
+    const { colorMode, toggleColorMode } = useColorMode(); // Use the color mode hook
 
-    return <Flex flexDir={'column'} minHeight={'100vh'} >
+    const bgColor = { light: 'gray.50', dark: 'gray.800' };
+    const color = { light: 'black', dark: 'white' };
+
+    return <Flex flexDir={'column'} minHeight={'100vh'} bg={bgColor[colorMode]} color={color[colorMode]} >
 
         <Flex className="header" h={'120px'} boxShadow={'0 4px 2px -2px #333'} alignItems={'center'}
-         width={'100%'} p={'35px'} gap={'35px'}>
+            width={'100%'} p={'35px'} gap={'35px'} justifyContent={'space-between'}>
+            <ButtonGroup>
                 <Button colorScheme='green' onClick={() => navigate('/')}>Home</Button>
                 {/* <Button colorScheme='green' onClick={() => navigate('/responsive-table')}>Responsive Table</Button> */}
                 <Button colorScheme='green' onClick={() => navigate('/basic-table')}>Basic Table</Button>
+            </ButtonGroup>
+            <IconButton
+                    icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                    isRound={true}
+                    size="md"
+                    alignSelf="flex-end"
+                    colorScheme='green'
+                    onClick={toggleColorMode}
+                    aria-label={`Switch to ${colorMode === 'light' ? 'dark' : 'light'} mode`}
+                />
         </Flex>
 
         <Flex flexGrow={'1'} >
